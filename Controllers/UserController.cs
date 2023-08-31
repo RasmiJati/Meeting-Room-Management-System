969617727495e0b7cc06e8f5a666846867669655
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-31 08:15:22 +0545

    created user controller and index view

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using MeetingRoomManagementSystem.Data;
+using MeetingRoomManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingRoomManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly MeetingDbContext _db;
+
+        public UserController(MeetingDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<User> users = _db.Users.ToList();
+            return View(users);
+        }
+    }
+}

18c2ad93754bfbb745dce826ef1a3f744b3836b5
Rasmi Jati <jatirasmi65@gmail.com>
2023-09-12 09:02:39 +0545

    performed crud operation for user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -28,6 +28,94 @@ namespace MeetingRoomManagementSystem.Controllers
         {
             return View();
         }
-    }
 
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken] // prevent the cross site frogery attacks
+        public IActionResult Create(User obj)
+        {
+            if (obj.Name == obj.Email)
+            {
+                //ModelState.AddModelError("CustomError","Name and email can't be same");
+                ModelState.AddModelError("name", "Name and email can't be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Successfully Created";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var user = _db.Users.Find(id);
+            //var userFirst=_db.Users.FirstOrDefault(x => x.id == id);
+            //var userSingle = _db.Users.SingleOrDefault(x => x.id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken] // prevent the cross site frogery attacks
+        public IActionResult Edit(User obj)
+        {
+            if (obj.Name == obj.Email)
+            {
+                //ModelState.AddModelError("CustomError","Name and email can't be same");
+                ModelState.AddModelError("name", "Name and email can't be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Users.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Successfully Edited ";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var user = _db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        //Post
+        [HttpPost, ActionName("Delete")]
+        //[HttpPost]
+        [ValidateAntiForgeryToken] // prevent the cross site frogery attacks
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Users.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Users.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Successfully Deleted ";
+            return RedirectToAction("Index");
+        }
+    }
 }

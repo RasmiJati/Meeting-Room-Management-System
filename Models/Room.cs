22a0b37109edc7da4d885c0216d09975b34b763a
Rasmi Jati <jatirasmi65@gmail.com>
2023-09-19 16:58:08 +0545

    remove status form user and remove migration

--- Models/Room.cs
+++ Models/Room.cs
@@ -6,6 +6,5 @@
         public string Name { get;set; }
         public string Capacity { get;set; }
         public string Description { get;set; }
-        public RoomStatus Status { get; set; }
     }
 }
